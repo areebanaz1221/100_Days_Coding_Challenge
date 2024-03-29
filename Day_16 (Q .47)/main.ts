71138c4ca5bae9087f9274a6ea34f5451c82a363
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 18:21:18 +0500

    Add files via upload

---
+++ Day_16 (Q .47)/main.ts
@@ -0,0 +1,9 @@
+let laptops = [
+    { make: "Lenovo", model: "t480", year: 2018 },
+    { make: "Apple", model: "Macbook 20", year: 2020 },
+    { make: "HP", model: "850 g5", year: 2018 }
+];
+let [laptop1, laptop2, laptop3] = laptops;
+console.log(laptop1);
+console.log(laptop2);
+console.log(laptop3);
