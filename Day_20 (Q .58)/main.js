ac1db1ee38c5da4a8920555aa1910ca462837ebf
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 18:36:34 +0500

    Add files via upload

---
+++ Day_20 (Q .58)/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// This program calculates the average of all scores given
+function averageScore(...scores) {
+    // Adds all scores together and divides by the number of scores
+    let total = scores.reduce((sum, score) => sum + score, 0);
+    return total / scores.length;
+}
+// Example: finding the average of four scores
+console.log(averageScore(80, 90, 100, 70, 50, 60));
