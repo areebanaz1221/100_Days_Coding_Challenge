59ad234d4b4b8897c161fde24466f4cb2164ee7d
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:26:35 +0500

    Add files via upload

---
+++ Day_26 (Q .77)/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function greetUser(name = "Areeba") {
+    console.log(`Hello, How are You? ${name}!`);
+}
+greetUser("Areeba");
+greetUser();
---
+++ Day_26 (Q .77)/main.ts
@@ -0,0 +1,10 @@
+
+
+function greetUser(name: string = "Areeba") {
+    
+    console.log(`Hello, How are You? ${name}!`);
+}
+
+
+greetUser("Areeba");
+greetUser();
