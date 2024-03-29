0875af5e278b07fbef558a94fa28cfb97a7212c3
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:35:39 +0500

    Add files via upload

---
+++ Day_28 (Q .83)/main.ts
@@ -0,0 +1,12 @@
+
+
+function convertCase(str: string) {
+    let upperStr = str.toUpperCase(); 
+    let lowerStr = str.toLowerCase(); 
+
+    console.log("Uppercase:", upperStr, "Lowercase:", lowerStr);
+
+}
+
+
+convertCase("Laughter Is The Best Policy!"); 
