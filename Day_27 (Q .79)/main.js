03e02000a3002750044412ca48c45fa6cc68fcdc
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:29:59 +0500

    Add files via upload

---
+++ Day_27 (Q .79)/main.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+let phone = {
+    make: "15 pro max",
+    model: "iphone",
+    year: 2023
+};
+console.log(chalk.bgMagentaBright(phone.model));
---
+++ Day_27 (Q .79)/main.ts
@@ -0,0 +1,10 @@
+import chalk from 'chalk'
+
+let phone = {
+    make: "15 pro max",
+    model:"iphone",
+    year: 2023
+};
+
+
+console.log(chalk.bgMagentaBright(phone.model));  
