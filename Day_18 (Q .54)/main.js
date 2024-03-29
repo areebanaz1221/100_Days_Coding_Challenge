b3cdde419041df979e485e9cbfcb41c5960c0b57
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 18:32:23 +0500

    Add files via upload

---
+++ Day_18 (Q .54)/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Object.keys() Method in Javascript
+const object1 = {
+    name: "Areeba",
+    age: "24",
+};
+console.log("Object 1", Object.keys(object1)); //  ["a", "b", "c"];
---
+++ Day_18 (Q .54)/main.ts
@@ -0,0 +1,10 @@
+
+// Object.keys() Method in Javascript
+
+const object1 = {
+    name: "Areeba",
+    age: "24",
+}
+
+console.log("Object 1", Object.keys(object1))   //  ["a", "b", "c"];
+
