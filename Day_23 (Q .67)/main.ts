ed6e5572fac5174c19833216372a78936d909c34
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:11:50 +0500

    Add files via upload

---
+++ Day_23 (Q .67)/main.ts
@@ -0,0 +1,9 @@
+
+function addNumberAndString(number1: number, numberString: string): number {
+    
+    return number1 + Number(numberString);
+}
+
+
+console.log(addNumberAndString(12, "6")); // 18
+
