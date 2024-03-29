2942001e30db667358b7c388ae66dfe4008bd05c
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:36:51 +0500

    Add files via upload

---
+++ Day_28 (Q .84)/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Replacing text in a string
+function replaceJavaScriptWithTypeScript(sentence) {
+    return sentence.replace(/JavaScript/g, "Coding");
+}
+console.log(replaceJavaScriptWithTypeScript("I love JavaScript!"));
---
+++ Day_28 (Q .84)/main.ts
@@ -0,0 +1,11 @@
+
+// Replacing text in a string
+
+
+function replaceJavaScriptWithTypeScript(sentence: string): string {
+    
+    
+    return sentence.replace(/JavaScript/g, "Coding");
+
+}
+console.log(replaceJavaScriptWithTypeScript("I love JavaScript!"));
