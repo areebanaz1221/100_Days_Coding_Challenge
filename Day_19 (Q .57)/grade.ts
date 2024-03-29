d9d975454a2f2e7e13b284629eda547d77dce81e
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 18:35:26 +0500

    Add files via upload

---
+++ Day_19 (Q .57)/grade.ts
@@ -0,0 +1,9 @@
+// A list of grades
+let grades = [86, 91, 52, 89, 58, 70];
+
+// Calculates the average grade
+let averageGrade = grades.reduce((total, grade) => total + grade, 0) / grades.length;
+
+// Shows the average grade
+console.log(averageGrade);
+
