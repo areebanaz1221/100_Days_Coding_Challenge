f50362538e54694d967f0fde734b9fcd635f8489
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 15:49:30 +0500

    Add files via upload

---
+++ Day_1 (Q .2)/Personal_message.js
@@ -0,0 +1,9 @@
+let personName = '';
+personName = prompt('what is your name?') || '';
+if (personName !== null && personName !== '') {
+    alert(`Hello ${personName}, would you like to learn some Python today?`);
+}
+else {
+    alert('You have to fill your name !');
+}
+export {};
