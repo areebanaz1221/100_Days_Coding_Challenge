991b8e33dac8e440c31ddeb9734cc6609f6c8d3b
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 18:41:08 +0500

    Add files via upload

---
+++ Day_20 (Q .59)/main.js
@@ -0,0 +1,17 @@
+"use strict";
+// Add a Specific numbers
+// 1 + 2 + 3 + 4 + 5 = 15
+// var sum = 0;
+// for (i = l;i <= 5; i++) {
+//     sum = sum + i 
+// }
+// console.log(sum)
+// sum of even number
+Object.defineProperty(exports, "__esModule", { value: true });
+var sum = 0;
+for (var i = 0; i <= 100; i++) {
+    if (i % 2 != 0) {
+        sum = sum + i;
+    }
+}
+console.log(sum);
