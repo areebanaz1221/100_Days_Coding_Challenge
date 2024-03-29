a3c9051f89ac74d4d9127996ea43ef88334bf878
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 18:33:50 +0500

    Add files via upload

---
+++ Day_19 (Q .55)/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Starts with a list of numbers
+let numbers = [7, 6, 8, 9, 10, 12];
+// Doubles each number
+let doubledNumbers = numbers.map(number => number * 2);
+// Shows the new list of doubled numbers
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
---
+++ Day_19 (Q .55)/main.ts
@@ -0,0 +1,8 @@
+// Starts with a list of numbers
+let numbers = [7, 6, 8, 9, 10, 12];
+
+// Doubles each number
+let doubledNumbers = numbers.map(number => number * 2);
+
+// Shows the new list of doubled numbers
+console.log(doubledNumbers); // Output: [ 14, 12, 16, 18, 20, 24 ]
