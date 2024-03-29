b75411a88673d7b3bacafa05a0d7436bbf1a383e
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 18:30:19 +0500

    Add files via upload

---
+++ Day_18 (Q .52)/main.ts
@@ -0,0 +1,19 @@
+// Sets up details about a smartphone
+
+
+let smartphone = {
+
+    make: "iphone",
+    model: "16 pro max",
+    specs: {
+
+        storage: "8 GB",
+        screenSize: "6.9",
+        batteryLife: "29 hours"
+
+    }
+
+};
+
+
+console.log(smartphone);
