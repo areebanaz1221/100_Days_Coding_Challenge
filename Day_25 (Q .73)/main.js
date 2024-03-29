fa8a38abeb5263ed9cb45300b0ea7597ac3db096
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:20:51 +0500

    Add files via upload

---
+++ Day_25 (Q .73)/main.js
@@ -0,0 +1,11 @@
+"use strict";
+var topic = "Javascript Variables!";
+console.log(topic);
+topic = "Updated Value!";
+console.log(topic);
+var apples = 0;
+apples += 1000;
+apples += 100;
+var mangoes = 200;
+var allTogether = apples + mangoes;
+console.log(allTogether);
