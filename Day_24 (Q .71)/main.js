dc7163e3003b5b959c6da1d1b1d3ff99b0484bbb
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:17:56 +0500

    Add files via upload

---
+++ Day_24 (Q .71)/main.js
@@ -0,0 +1,7 @@
+"use strict";
+const age = 23;
+console.log(age);
+let a = "Areeba";
+console.log(a);
+a = "Areebajput";
+console.log(a);
---
+++ Day_24 (Q .71)/main.ts
@@ -0,0 +1,10 @@
+
+const age = 23;
+console.log(age);
+
+
+let a = "Areeba";
+console.log(a);
+
+a = "AreebaRajput";
+console.log(a)
