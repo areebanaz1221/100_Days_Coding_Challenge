ea9281428c2ce0f0f240787b7e7eef294d4107ca
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:32:19 +0500

    Add files via upload

---
+++ Day_27 (Q .81)/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let phoneProperties = {
+    make: "15 pro max",
+    model: "iphone",
+    year: 2023,
+    colour: "black",
+};
+for (let properties in phoneProperties) {
+    console.log(phoneProperties);
+}
---
+++ Day_27 (Q .81)/main.ts
@@ -0,0 +1,13 @@
+let phoneProperties = {
+
+    
+    make: "15 pro max",
+    model: "iphone",
+    year: 2023,
+    colour: "black",
+ 
+};
+
+for (let properties in phoneProperties) {
+ console.log(phoneProperties);
+}
