73257b0828a193c995c7ad7ad65bfeb70776a99f
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 18:45:47 +0500

    Add files via upload

---
+++ Day_21 (Q .62)/main.ts
@@ -0,0 +1,15 @@
+
+
+let Student = {
+
+
+    name: "Areeba",
+    age: 23,
+    courses: ["Artificial Intelligence(AI)", "Google Cloud", "Python Programming", "IBM", "Technology"]
+
+
+};
+
+
+console.log(Student);
+// We're using the blueprint to make sure our student has a name, age, and list of courses.
