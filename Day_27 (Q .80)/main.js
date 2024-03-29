7773fe236ba3c6a28357d7cd5755efbf43f40e05
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:31:19 +0500

    Add files via upload

---
+++ Day_27 (Q .80)/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let phone = {
+    make: "15 pro max",
+    model: "iphone",
+    year: 2022,
+    color: "black"
+};
+phone.color = "gray";
+phone.year = 2023;
+console.log(phone);
---
+++ Day_27 (Q .80)/main.ts
@@ -0,0 +1,14 @@
+
+let phone = {
+    make: "15 pro max",
+    model: "iphone",
+    year: 2022,
+    color: "black"
+};
+
+
+phone.color = "gray";
+phone.year = 2023; 
+
+
+console.log(phone); 
