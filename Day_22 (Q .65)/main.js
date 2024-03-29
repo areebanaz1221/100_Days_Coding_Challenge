81024e91ce2449ea6690f4cebf5a9990ca2164e3
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:07:59 +0500

    Add files via upload

---
+++ Day_22 (Q .65)/main.js
@@ -0,0 +1,5 @@
+"use strict";
+function remainder(num1, num2) {
+    return num1 % num2;
+}
+console.log(remainder(7, 3)); // 1
---
+++ Day_22 (Q .65)/main.ts
@@ -0,0 +1,8 @@
+
+function remainder(num1: number, num2: number): number {
+
+    return num1 % num2;
+}
+
+
+console.log(remainder(7, 3)); // 1
