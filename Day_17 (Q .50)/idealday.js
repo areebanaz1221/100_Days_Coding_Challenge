0a955daa257ab8d4c88bb3cb4969f79cc1606295
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 18:24:42 +0500

    Add files via upload

---
+++ Day_17 (Q .50)/idealday.js
@@ -0,0 +1,21 @@
+"use strict";
+const idealDay = `
+Morning:
+- Start the day with a hearty breakfast, enjoying paratha made by mom's hands with a steaming cup of tea.
+- Practice mindfulness and gratitude through meditation, setting positive intentions for the day ahead.
+
+Afternoon:
+- Embark on a scenic hike in the mountains, surrounded by lush greenery and breathtaking views.
+- Have a picnic by a serene lake, indulging in delicious sandwiches and homemade snacks.
+
+Evening:
+- Ending the day by reading a good novel.
+`;
+console.log(idealDay);
+// Using template literals to define a multiline string
+let myIdealDay = `My ideal day would involve:
+1. Waking up early and going for a jog.
+2. Spending a few hours coding on a personal project.
+3. Ending the day by reading a good book.`;
+// Logging the multiline string to the console
+console.log(myIdealDay);
---
+++ Day_17 (Q .50)/idealday.ts
@@ -0,0 +1,16 @@
+
+const idealDay = `
+Morning:
+- Start the day with a hearty breakfast, enjoying paratha made by mom's hands with a steaming cup of tea.
+- Practice mindfulness and gratitude through meditation, setting positive intentions for the day ahead.
+
+Afternoon:
+- Embark on a scenic hike in the mountains, surrounded by lush greenery and breathtaking views.
+- Have a picnic by a serene lake, indulging in delicious sandwiches and homemade snacks.
+
+Evening:
+- Ending the day by reading a good novel.
+`;
+
+console.log(idealDay);
+
