e5c7dc94b72516070b80d1a381c944c29717011e
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:21:56 +0500

    Add files via upload

---
+++ Day_25 (Q .74)/main.ts
@@ -0,0 +1,13 @@
+
+function swapValues() {
+    let a = 5, b = 10; 
+    console.log("Before swap: a =", a, "b =", b);
+
+    let temp = a;
+    a = b; 
+    b = temp; 
+
+    console.log("After swap: a =", a, "b =", b); 
+}
+
+swapValues();
