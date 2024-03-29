591ff8d1e6f13d5bfcfb45cdc3fd543a509004d9
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 16:18:54 +0500

    Add files via upload

---
+++ Day_4 (Q .10)/program_1.ts
@@ -0,0 +1,8 @@
+
+
+// Program Written by: Areeba
+// Current Date: 7-3-2024
+
+// This program will write Hello World
+
+console.log('Hello World');
---
+++ Day_4 (Q .10)/program_2.ts
@@ -0,0 +1,8 @@
+
+
+// Program Written by: Areeba
+// Current Date: 7-3-2024
+
+// This program will add two digits
+
+console.log(2+2);
