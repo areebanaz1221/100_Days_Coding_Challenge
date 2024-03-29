a98aed4acb288ac09dce1ab2aee4100f4e959b2f
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 16:33:42 +0500

    Add files via upload

---
+++ Day_4 (Q .12)/main.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Rabia', 'Khansa', 'Areeba'];
+const message = 'Always be happy. ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Day_4 (Q .12)/main.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] = ['Rabia','Khansa','Areeba'] ;
+
+const message :string = 'Always be happy. '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+    
+}
