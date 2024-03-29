25c7c8e98859568b64039cfeff257730580c426e
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 19:09:17 +0500

    Add files via upload

---
+++ Day_22 (Q .66)/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// This function sees if both inputs are true
+function checkBothFalse(val1, val2) {
+    return val1 && val2;
+}
+// Trying it with true and false
+console.log(checkBothFalse(true, false)); // Shows false
+// It checks two things, but since one is false, the answer is false.
