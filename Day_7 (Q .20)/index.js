c8d9ad995ae356642c9e5de7f25f8ae0fbd4e14b
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 16:44:27 +0500

    Add files via upload

---
+++ Day_7 (Q .20)/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const countries = ["America", "Pakistan", "India", "China"];
+console.log("List of Countries:");
+for (let country of countries) {
+    console.log(country);
+}
---
+++ Day_7 (Q .20)/index.ts
@@ -0,0 +1,9 @@
+
+const countries: string[] = ["America", "Pakistan", "India", "China"];
+
+console.log("List of Countries:")
+
+for (let country of countries)
+{
+    console.log(country)
+}
