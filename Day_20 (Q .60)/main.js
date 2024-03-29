ef1bddd23398a32b36755ba1c7bd6f6f67d606b2
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-29 18:42:20 +0500

    Add files via upload

---
+++ Day_20 (Q .60)/main.js
@@ -0,0 +1,11 @@
+"use strict";
+let userProfile = (function () {
+    let name = "Areeba";
+    let age = 24;
+    return {
+        displayInfo: function () {
+            console.log(`Name: ${name}, Age: ${age}`);
+        }
+    };
+})();
+userProfile.displayInfo();
