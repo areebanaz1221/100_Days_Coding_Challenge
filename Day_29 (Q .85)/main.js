e088d30f126704914d98f454ad216fb66e1bde46
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-30 15:27:12 +0500

    Add files via upload

---
+++ Day_29 (Q .85)/main.js
@@ -0,0 +1,5 @@
+"use strict";
+function findCodePosition(str) {
+    return str.indexOf("Areeba");
+}
+console.log(findCodePosition("Coding with Areeba Ashraf"));
---
+++ Day_29 (Q .85)/main.ts
@@ -0,0 +1,11 @@
+
+
+function findCodePosition(str: string): number {
+      
+    
+    return str.indexOf("Areeba"); 
+
+
+}
+
+console.log(findCodePosition("Coding with Areeba Ashraf")); 
