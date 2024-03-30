d4d9a89158bed193f5b4fa4e4e5d665fa5888dcb
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-30 15:28:01 +0500

    Add files via upload

---
+++ Day_29 (Q .86)/main.js
@@ -0,0 +1,5 @@
+"use strict";
+function hasJavaScript(str) {
+    return str.includes("coding");
+}
+console.log(hasJavaScript("I love coding in JavaScript!"));
---
+++ Day_29 (Q .86)/main.ts
@@ -0,0 +1,10 @@
+
+
+function hasJavaScript(str: string): boolean {
+    
+    
+    return str.includes("coding");
+
+}
+
+console.log(hasJavaScript("I love coding in JavaScript!")); 
