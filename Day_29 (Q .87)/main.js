97535544f10c550bb60cb07dbf2a2e90eabdf523
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-30 15:28:55 +0500

    Add files via upload

---
+++ Day_29 (Q .87)/main.js
@@ -0,0 +1,5 @@
+"use strict";
+function extractFirstTenChars(str) {
+    return str.substring(0, 8);
+}
+console.log(extractFirstTenChars("Hello, JavaScript world!"));
---
+++ Day_29 (Q .87)/main.ts
@@ -0,0 +1,11 @@
+
+
+function extractFirstTenChars(str: string): string {
+ 
+    return str.substring(0, 8
+        ); 
+
+}
+
+
+console.log(extractFirstTenChars("Hello, JavaScript world!"));
