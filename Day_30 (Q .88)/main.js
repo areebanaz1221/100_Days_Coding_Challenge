f8522975dc9c6ccc6adac64e9ef8ba9a9b6f9bfe
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-31 11:18:43 +0500

    Add files via upload

---
+++ Day_30 (Q .88)/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// const x = 20.25
+// const y = Math.round(x);
+// console.log(y); // 20
+const x = 20.75;
+const y = Math.round(x);
+console.log(y);
---
+++ Day_30 (Q .88)/main.ts
@@ -0,0 +1,13 @@
+
+// const x = 20.25
+
+// const y = Math.round(x);
+
+// console.log(y); // 20
+
+
+const x = 20.75
+
+const y = Math.round(x);
+
+console.log(y);
