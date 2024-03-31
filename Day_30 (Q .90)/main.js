f448fb002edbb6d7b8041c59d1eaa110c86ab1a1
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-03-31 11:23:40 +0500

    Add files via upload

---
+++ Day_30 (Q .90)/main.js
@@ -0,0 +1,6 @@
+"use strict";
+function isValueNaN(value) {
+    return isNaN(value);
+}
+console.log(isValueNaN("Hello World!"));
+console.log(isValueNaN(12345));
---
+++ Day_30 (Q .90)/main.ts
@@ -0,0 +1,9 @@
+
+
+function isValueNaN(value: any): boolean {
+    return isNaN(value); 
+}
+
+
+console.log(isValueNaN("Hello World!")); 
+console.log(isValueNaN(12345));
