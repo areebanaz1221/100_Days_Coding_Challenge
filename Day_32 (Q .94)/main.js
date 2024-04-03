8c2d5fbb0f36cbf5669504a0d531b398975c6938
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-04 04:58:11 +0500

    Add files via upload

---
+++ Day_32 (Q .94)/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Define an Array lenght
+const words = ["CSS", "HTML", "Canva", "Edobe illustrator", "Javascript", "Typescript"];
+// 4,  // 4,    // 5,         // 16,             // 10,      // 10   
+const lengths = words.map(word => word.length);
+console.log(lengths);
---
+++ Day_32 (Q .94)/main.ts
@@ -0,0 +1,12 @@
+
+// Define an Array lenght
+
+
+const words: string[] = ["CSS", "HTML", "Canva", "Edobe illustrator", "Javascript","Typescript"];
+                        // 3,  // 4,    // 5,         // 17,             // 10,      // 10   
+
+
+const lengths: number[] = words.map(word => word.length);
+
+
+console.log(lengths); 
