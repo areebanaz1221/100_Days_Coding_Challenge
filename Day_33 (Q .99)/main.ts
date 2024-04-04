d1a5522958030f63810cb0d20f41e8676bc3f481
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-04 05:02:27 +0500

    Add files via upload

---
+++ Day_33 (Q .99)/main.ts
@@ -0,0 +1,14 @@
+
+function getNextBirthday(month: number, day: number): Date {
+    const today = new Date();
+    let year = today.getFullYear();
+    const birthday = new Date(year, month - 1, day); 
+    if (birthday < today) {
+    
+        birthday.setFullYear(year + 1);
+    }
+    return birthday;
+}
+
+const nextBirthday = getNextBirthday(7, 26); 
+console.log("Next birthday on:", nextBirthday.toLocaleDateString());
