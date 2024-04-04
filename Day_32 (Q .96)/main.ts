d8d32c895b748adf9679c3e1aa8224c713420909
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-04 05:00:10 +0500

    Add files via upload

---
+++ Day_32 (Q .96)/main.ts
@@ -0,0 +1,10 @@
+
+function calculateSum(numbers: number[]): number {
+
+    return numbers.reduce((accumulator, current) => accumulator + current, 0);
+    
+}
+
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+console.log(calculateSum(numbers)); 
