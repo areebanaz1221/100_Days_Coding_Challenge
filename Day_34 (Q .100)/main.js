03fdc4a772be87f42602eb9632801b17adf57e0b
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-05 11:22:03 +0500

    Add files via upload

---
+++ Day_34 (Q .100)/main.js
@@ -0,0 +1,4 @@
+"use strict";
+// Finds the square root of 144 using Math.sqrt()
+const squareRoot = Math.sqrt(121);
+console.log(squareRoot); // 11
---
+++ Day_34 (Q .100)/main.ts
@@ -0,0 +1,5 @@
+
+// Finds the square root of 121 using Math.sqrt()
+const squareRoot: number = Math.sqrt(121)
+
+console.log(squareRoot); // 11
