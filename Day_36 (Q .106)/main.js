70d1ed1ebc5b919c72eb1dd06c7c45cec9a90585
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-06 11:11:38 +0500

    Add files via upload

---
+++ Day_36 (Q .106)/main.js
@@ -0,0 +1,16 @@
+"use strict";
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+let year = 2023;
+if (isLeapYear(year)) {
+    console.log(year + " is a leap year.");
+}
+else {
+    console.log(year + " is not a leap year.");
+}
---
+++ Day_36 (Q .106)/main.ts
@@ -0,0 +1,16 @@
+function isLeapYear(year: number) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+// let year = 2023;
+let year = 2024;
+if (isLeapYear(year)) {
+    console.log(year + " is a leap year.");
+} else {
+    console.log(year + " is not a leap year.");
+}
