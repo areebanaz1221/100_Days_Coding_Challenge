b5f50371d317795ae3e6cce8cc54cb8fd5ea7250
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-06 11:13:56 +0500

    Add files via upload

---
+++ Day_36 (Q .107)/main.ts
@@ -0,0 +1,15 @@
+function isDivisibleBy2And3(number: number) {
+    if (number % 2 === 0 && number % 3 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Example usage
+let num = 12;
+if (isDivisibleBy2And3(num)) {
+    console.log(num + " is divisible by both 2 and 3.");
+} else {
+    console.log(num + " is not divisible by both 2 and 3.");
+}
