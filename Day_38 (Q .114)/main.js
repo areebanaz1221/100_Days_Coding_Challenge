5da0b8e770ed3d2d2fba930fe3f24bfada9cdece
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-09 10:53:18 +0500

    Add files via upload

---
+++ Day_38 (Q .114)/main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Creating a Map of student IDs and names
+const studentMap = new Map();
+studentMap.set(1, "Rabia");
+studentMap.set(2, "Khansa");
+studentMap.set(3, "Areeba");
+studentMap.set(4, "Zaid");
+studentMap.set(5, "Duraid");
+studentMap.forEach((name, id) => {
+    console.log(`Student ID: ${id}, Name: ${name}`);
+});
