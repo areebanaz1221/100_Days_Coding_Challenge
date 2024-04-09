bc96621d279bb4cf92c3c2f42f132a35d4083e53
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-09 10:52:28 +0500

    Add files via upload

---
+++ Day_38 (Q .113)/main.ts
@@ -0,0 +1,16 @@
+function checkForCanada(map: Map<any, any>) {
+    if (map.has("Canada")) {
+        console.log(`The capital of Canada is ${map.get("Canada")}.`);
+    } else {
+        console.log("Canada is not found in the map.");
+    }
+}
+
+// Example usage:
+let countryCapitalMap = new Map();
+countryCapitalMap.set("United States", "Washington, D.C.");
+countryCapitalMap.set("United Kingdom", "London");
+countryCapitalMap.set("France", "Paris");
+
+checkForCanada(countryCapitalMap);
+``
