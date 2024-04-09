d828deb4d90c56e569c932b096b14a4dc979a662
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-09 10:51:42 +0500

    Add files via upload

---
+++ Day_38 (Q .112)/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Create a Map to store country-capital pairs
+let countryCapitalMap = new Map();
+// Add countries and their capitals to the Map
+countryCapitalMap.set("United States", "Washington, D.C.");
+countryCapitalMap.set("United Kingdom", "London");
+countryCapitalMap.set("France", "Paris");
+// Display the map
+console.log("Country-Capital Map:");
+countryCapitalMap.forEach((capital, country) => {
+    console.log(`${country} - ${capital}`);
+});
