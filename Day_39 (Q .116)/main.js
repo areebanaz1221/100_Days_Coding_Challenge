32a3a8539e93545141335d1bde6439e9c35f471b
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-09 10:54:56 +0500

    Add files via upload

---
+++ Day_39 (Q .116)/main.js
@@ -0,0 +1,27 @@
+"use strict";
+function getSeason(month) {
+    switch (month) {
+        case 1:
+        case 2:
+        case 12:
+            return "Winter";
+        case 3:
+        case 4:
+        case 5:
+            return "Spring";
+        case 6:
+        case 7:
+        case 8:
+            return "Summer";
+        case 9:
+        case 10:
+        case 11:
+            return "Autumn";
+        default:
+            return "Invalid month";
+    }
+}
+// Example usage:
+const month = 6;
+const season = getSeason(month);
+console.log(`The season for month ${month} is ${season}`);
