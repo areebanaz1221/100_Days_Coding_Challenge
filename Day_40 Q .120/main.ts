2c6167011d7adb139a8e791ce921e5e904ca0f21
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-12 13:40:17 +0500

    Add files via upload

---
+++ Day_40 Q .120/main.ts
@@ -0,0 +1,5 @@
+const favoriteMovies = ["The Shawshank Redemption", "The Godfather", "The Dark Knight", "Pulp Fiction", "Forrest Gump"];
+
+for (const movie of favoriteMovies) {
+  console.log(movie);
+}
