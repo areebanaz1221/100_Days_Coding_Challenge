501a14ac4909f780f227edd337180edf5a7dbc9d
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-12 13:44:12 +0500

    Add files via upload

---
+++ Day_41 Q .123/main.js
@@ -0,0 +1,10 @@
+"use strict";
+let str = "example string";
+let vowels = "aeiou";
+for (let i = 0; i < str.length; i++) {
+    let char = str[i].toLowerCase();
+    if (vowels.includes(char)) {
+        console.log("First vowel found:", char);
+        break;
+    }
+}
