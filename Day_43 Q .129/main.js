3b7f309daa8e7a48f00898f91946b5054a4197b3
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-14 13:48:09 +0500

    Add files via upload

---
+++ Day_43 Q .129/main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Traditional Function
+// Expression
+let x = function (a, b) {
+    return a + b;
+};
+console.log(x(5, 6));
+// Arrow Function
+// Expression
+let y = (c, d) => {
+    return c + d;
+};
+console.log(y(5, 6));
---
+++ Day_43 Q .129/main.ts
@@ -0,0 +1,17 @@
+// Traditional Function
+// Expression
+
+let x = function(a: any,b: any) {
+  return a+b;
+}
+
+console.log(x(5,6));
+
+// Arrow Function
+// Expression
+
+let y = (c: any,d: any) => {
+  return c+d;
+}
+
+console.log(y(5,6));
