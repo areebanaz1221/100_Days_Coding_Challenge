b8f1ede05a2f3440080dc50c3ce70059315a4bf6
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-14 13:40:40 +0500

    Delete DAy_41 Q .122 directory

--- DAy_41 Q .122/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-let i = 10;
-while (i >= 1) {
-    console.log(i);
-    if (i === 5) {
-        break;
-    }
-    i--;
-}
--- DAy_41 Q .122/main.ts
+++
@@ -1,8 +0,0 @@
-let i = 10;
-while (i >= 1) {
-    console.log(i);
-    if (i === 5) {
-        break;
-    }
-    i--;
-}
