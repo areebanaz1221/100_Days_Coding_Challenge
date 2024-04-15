596e56e114048cee46e5b4c749eff94501590b10
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-15 19:56:52 +0500

    Add files via upload

---
+++ Day_45 Q .133/main.js
@@ -0,0 +1,10 @@
+"use strict";
+const j = {
+    name: "Areeba",
+    age: 23,
+    hobbies: ["Coding", "Reading", "Travelling"],
+    country: "Pakistan",
+    city: "Karachi",
+};
+const jsonString = JSON.stringify(j);
+console.log(jsonString);
---
+++ Day_45 Q .133/main.ts
@@ -0,0 +1,12 @@
+const j = {
+    
+    name: "Areeba",
+    age: 23,
+    hobbies: ["Coding", "Reading", "Travelling"],
+    country: "Pakistan",
+    city: "Karachi",
+};
+
+const jsonString = JSON.stringify(j);
+
+console.log(jsonString);
