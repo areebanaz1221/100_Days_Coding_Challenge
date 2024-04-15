6830986bd8a9b0b5a5ad097fb47358daf82420c2
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-15 19:57:38 +0500

    Add files via upload

---
+++ Day_45 Q .134/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let jsonString = '{"name": "Areeba", "age": 23, "address": {"city": "Karachi", "country": "Pakistan"}}';
+let person = JSON.parse(jsonString);
+console.log(person);
---
+++ Day_45 Q .134/main.ts
@@ -0,0 +1,8 @@
+let jsonString = '{"name": "Areeba", "age": 23, "address": {"city": "Karachi", "country": "Pakistan"}}';
+
+
+let person = JSON.parse(jsonString);
+
+
+
+console.log(person);
