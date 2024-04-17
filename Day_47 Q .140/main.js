318a4363895b7ff63bbe434f6af072846eac07a2
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-17 20:24:26 +0500

    Add files via upload

---
+++ Day_47 Q .140/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let;
+if ( = 5)
+    ; // This line would cause a syntax error
+// console.log(
+//     "Using a reserved word as a variable name causes a syntax error in JavaScript."
+//   );
---
+++ Day_47 Q .140/main.ts
@@ -0,0 +1,5 @@
+let if = 5; // This line would cause a syntax error
+
+// console.log(
+//     "Using a reserved word as a variable name causes a syntax error in JavaScript."
+//   );
