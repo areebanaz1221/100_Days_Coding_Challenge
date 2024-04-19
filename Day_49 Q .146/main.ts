afb05da3b7ade9bbd130e88868f6d366b1967fe5
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-19 18:45:47 +0500

    Add files via upload

---
+++ Day_49 Q .146/main.ts
@@ -0,0 +1,21 @@
+// Callback function to filter even number
+function filterEvenNumbers(number: any) {
+    return number % 2 === 0;
+}
+
+// Function to filter an array based on a callback
+function filterArray(array: any, callback: any) {
+    const filteredArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            filteredArray.push(array[i]);
+        }
+    }
+    return filteredArray;
+}
+
+// Example usage:
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const evenNumbers = filterArray(numbers, filterEvenNumbers);
+console.log("Even numbers:", evenNumbers);
