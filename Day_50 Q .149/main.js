ab8c1f628f04ef2ef5fb81e1c9fbdfd2e73ae0ae
areebanaz1221 <159836654+areebanaz1221@users.noreply.github.com>
2024-04-20 18:58:24 +0500

    Add files via upload

---
+++ Day_50 Q .149/main.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Start");
+// Asynchronous operation: setTimeout
+setTimeout(function () {
+    console.log("Asynchronous operation completed");
+}, 2000); // Wait for 2 seconds
+console.log("End");
---
+++ Day_50 Q .149/main.ts
@@ -0,0 +1,8 @@
+console.log("Start");
+
+// Asynchronous operation: setTimeout
+setTimeout(function() {
+  console.log("Asynchronous operation completed");
+}, 2000); // Wait for 2 seconds
+
+console.log("End");
